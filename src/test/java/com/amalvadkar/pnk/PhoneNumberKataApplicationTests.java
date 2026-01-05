d51646ee3d449f72f2c7bdef2622701ca1a0a5fa
Abhishek <abhi@dev.com>
2026-01-05 12:22:07 +0530

    Initial commit

---
+++ src/test/java/com/amalvadkar/pnk/PhoneNumberKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.pnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumberKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
